714e44994f378b75c1475f92e4695bdef7128ca6
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2025-07-01 16:32:20 -0700

    Add a button

--- script.jl
+++ script.jl
@@ -2,9 +2,23 @@ using Bonito
 using WGLMakie
 using DataStructures
 
-buf1 = Observable(CircularBuffer{Int16}(100))
-buf2 = Observable(CircularBuffer{Int16}(100))
-buf3 = Observable(CircularBuffer{Int16}(100))
+# Initialize buffers with some initial data
+initial_buf1 = CircularBuffer{Int16}(100)
+initial_buf2 = CircularBuffer{Int16}(100)
+initial_buf3 = CircularBuffer{Int16}(100)
+
+# Fill with initial data
+for i in 1:10
+    push!(initial_buf1, rand(Int16))
+    push!(initial_buf2, rand(Int16))
+    push!(initial_buf3, rand(Int16))
+end
+
+buf1 = Observable(initial_buf1)
+buf2 = Observable(initial_buf2)
+buf3 = Observable(initial_buf3)
+
+is_running = Observable(false)
 
 fig = Figure();
 ax1 = Axis(fig[1,1],
@@ -30,23 +44,33 @@ ax3 = Axis(fig[3,1], title="Histogram of Lastest 100 points")
 stephist!.(ax3, [buf1, buf2, buf3], bins=(typemin(Int16):1000:typemax(Int16)))
 
 t = Threads.@spawn try while true
-        push!(buf1.val, rand(Int16))
-        push!(buf2.val, rand(Int16))
-        push!(buf3.val, rand(Int16))
-        notify(buf1)
-        notify(buf2)
-        notify(buf3)
-        Makie.reset_limits!.([ax1, ax2, ax3])
+        if is_running[]
+            push!(buf1.val, rand(Int16))
+            push!(buf2.val, rand(Int16))
+            push!(buf3.val, rand(Int16))
+            notify(buf1)
+            notify(buf2)
+            notify(buf3)
+            Makie.reset_limits!.([ax1, ax2, ax3])
+        end
         sleep(1)
     end
 catch e
     e isa InterruptionException ? (@info "Stopped" exception = e) : rethrow(e)
 end
 
+toggle_button = Button("Start Display")
+
+on(toggle_button.value) do _
+    is_running[] = !is_running[]
+    toggle_button.content[] = is_running[] ? "Stop Display" : "Start Display"
+end
+
 app = App(
     DOM.div(
         DOM.h1("Values from Pressure Gauges (fake for now)"),
         DOM.p("Styles can be adjusted later :)"),
+        toggle_button,
         fig
     )
 )
